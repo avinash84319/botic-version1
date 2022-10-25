1c2570575a27e15c7cc802cfe131703d44854b8d
avinash84319 <avichinnu22@gmail.com>
2022-10-26 03:47:07 +0530

    all changes working for custome name and basic customer page

--- app.js
+++ app.js
@@ -9,30 +9,54 @@ app.set("view engine","ejs") // to tell browser that ejs is bieng used
 const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/boticdb",{useNewUrlParser:true});
 
-
-var customers=["AVINASH","VAISHU","NANA"];
-var addcustomer;
 var customer;
+
+const scheme=mongoose.Schema({
+    name:String,
+    phoneno:Number,
+    products:[]
+})
+
+var customers=mongoose.model("customers",scheme);
 
 
 app.get("/", function(req,res){      //sendingg response on home route
-    res.render("index",{customers:customers}); //sending array to ejs file
+    customers.find({},function(err,customers){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.render("index", {customers:customers});
+        }
+      }); //sending array to ejs file
 })
 
 app.get("/customerpage", function(req,res){      //sendingg response on home route
-    res.render("customer",{customer:customer,customers:customers}); //sending array to ejs file
+    customers.find({phoneno:{$eq:customer}},function(err,customerpage){
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log(customerpage[0]);
+          res.render("customer", {customer:customerpage[0]});
+        }
+      });//sending array to ejs file
 })
 
 
 app.post("/",function(req,res){
-    if(addcustomer!=req.body.addcustomer)
-    addcustomer=req.body.addcustomer; 
-    customers.push(addcustomer);           // apending new customer to old list
+    const addcustomer=new customers({          // apending new customer to old list
+        name:req.body.addcustomer,
+        phoneno:req.body.addphone,
+        products:[]
+    }) 
+    addcustomer.save();                                        
     res.redirect("/"); //again sending ejs with new customer list
 })
 
 app.post("/customerpage",function(req,res){
-    customer=req.body.customername;
+    customer=req.body.customerphoneno;
+    console.log(req.body.customerphoneno);
     res.redirect("/customerpage");  //goes to personal customer page or customer.ejs
 })
 
