b700091f062c696db9df2098476b0a1ef0a182d2
avinash84319 <avichinnu22@gmail.com>
2022-10-26 11:12:49 +0530

    adding products adding,delting functions

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/boticdb",{useNewUrlParser:true});
 
 var customer;
+var product;
 
-const scheme=mongoose.Schema({
+const scheme=mongoose.Schema({     //schema for customers object in customer collection
     name:String,
     phoneno:Number,
     products:[]
 })
 
-var customers=mongoose.model("customers",scheme);
+var customers=mongoose.model("customers",scheme);  //created the collection customers
 
 
 app.get("/", function(req,res){      //sendingg response on home route
@@ -53,13 +54,13 @@ app.post("/",function(req,res){
     res.redirect("/"); //again sending ejs with new customer list
 })
 
-app.post("/customerpage",function(req,res){
+app.post("/customerpage",function(req,res){  //going to customer page
     customer=req.body.customerphoneno;
     console.log(req.body.customerphoneno);
     res.redirect("/customerpage");  //goes to personal customer page or customer.ejs
 })
 
-app.post("/delete",function(req,res){
+app.post("/delete",function(req,res){    //delete customer
     customer=req.body.customerphoneno;                              //store the phoneno of customer to be deleted
     console.log(customer);
     customers.deleteOne( {phoneno:{$eq:customer}},function(err){     //deleting function annd redirecting to home route
@@ -70,6 +71,38 @@ app.post("/delete",function(req,res){
         res.redirect("/");
       }
     });
+})
+
+app.post("/addproduct",function(req,res){  //adding product
+  customer=req.body.phoneno;
+  console.log(req.body.phoneno);   //recognising which customer to add product
+   var addproduct={                          //object product adding to products array
+    name:req.body.addproduct,
+    work:req.body.addstage,
+    price:req.body.addprice
+  };
+  customers.updateOne({phoneno:customer},{$push:  //pushing new object into prouct array
+  {products:addproduct}},function(err){                           //error function always to coded to mongodb to work
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect("/customerpage");
+    }
+  });
+
+})
+
+app.post("/deleteproduct",function(req,res){   //deleting product with checkbox
+    product=req.body.deleteproduct;
+    customers.updateOne({phoneno:customer},{$pull:{products:{name:product}}},function(err){  //mathing for customer from previous customer vaiable
+      if(err){                                               //matching name with name of product
+        console.log(err);                                         //pull deletes the object from array product
+      }
+      else{
+        res.redirect("/customerpage")
+      }
+    })
 })
 
 
